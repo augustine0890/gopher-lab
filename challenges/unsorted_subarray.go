3329222e3c3fc5abcc70a3339577d1d12c82d0ed
augustine <augustino0890@gmail.com>
2023-12-04 15:27:20 +0900

    shortest unsorted continuous subarray

---
+++ challenges/unsorted_subarray.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Time: O(N) and Space: O(1)
+func findUnsortedSubarray(nums []int) int {
+	left, right, max := 1, 1, nums[0]
+	for i := 1; i < len(nums)-1; i++ {
+		if nums[i] > max {
+			max = nums[i]
+		}
+
+		// increasing until num[:left] is correctly sorted
+		if left == i && nums[i] > nums[i-1] {
+			left++
+			right++
+		} else if nums[i] < max {
+			// subsequent element should be equal to or greater the current maximum so far,
+			// if not the marked as unsorted elements
+			right = i + 1
+		}
+
+		for left > 0 && nums[left-1] > nums[i] {
+			left--
+		}
+	}
+
+	return right - left
+
+}
+
+func main() {
+	fmt.Println(findUnsortedSubarray([]int{2, 6, 4, 8, 10, 9, 15}))
+	fmt.Println(findUnsortedSubarray([]int{2, 6, 4, 8, 10, 15}))
+	fmt.Println(findUnsortedSubarray([]int{1, 2, 3, 4}))
+	fmt.Println(findUnsortedSubarray([]int{1}))
+
+}
